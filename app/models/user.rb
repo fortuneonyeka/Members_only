c0c18a5717d4b7aeb9340226b68b8d39566d8c62
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-23 19:01:03 +0100

    add post model,validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
 end
