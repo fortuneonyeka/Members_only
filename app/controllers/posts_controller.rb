bd127c846338f5ed01a351cc8e7339266cef8f23
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-23 22:13:29 +0100

    update sign up,Post#new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :new]
+
   def index
     @posts = Post.all
   end
 
   def create; end
 
-  def new; end
+  def new
+    @post = Post.new
+   end
 
   def show; end
 end
