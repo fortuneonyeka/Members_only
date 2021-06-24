11aa53e58b358e67c1a85f8cc3867bf9aa62ec28
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-24 16:22:01 +0100

    update login,sign up and sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    delete "/log_out" => "devise/sessions#destroy"
+    get "/sign_up" => "devise/registrations#new" # custom path to sign_up/registration
+  end
 
   resources :posts, only: [:index, :new, :create, :show]
+  # Below for all other routes:
+  devise_for :users
 end
